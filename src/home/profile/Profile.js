8187bb624a106fc896951584ddb5e99a44545b59
William Candillon <wcandillon@gmail.com>
2017-12-20 15:44:40 +0100

    :wrench:

--- src/home/profile/Profile.js
+++ src/home/profile/Profile.js
@@ -1,13 +1,24 @@
 // @flow
 import autobind from "autobind-decorator";
 import * as React from "react";
-import {View, StyleSheet, Dimensions, FlatList, TouchableOpacity, SafeAreaView, Image} from "react-native";
+import {View, StyleSheet, Dimensions, FlatList, TouchableOpacity, Animated, Image} from "react-native";
+import {Constants} from "expo";
 import {Feather as Icon} from "@expo/vector-icons";
 
 import {Text, APIStore, Avatar, NavigationHelpers, Theme, Post, FirstPost, Images} from "../../components";
 import type {ScreenProps} from "../../components/Types";
 
-export default class Profile extends React.Component<ScreenProps<>> {
+type ProfileState = {
+    scrollAnimation: Animated.Value
+};
+
+export default class Profile extends React.Component<ScreenProps<>, ProfileState> {
+
+    componentWillMount() {
+        this.setState({
+            scrollAnimation: new Animated.Value(0)
+        });
+    }
 
     @autobind
     logout() {
@@ -15,99 +26,95 @@ export default class Profile extends React.Component<ScreenProps<>> {
         NavigationHelpers.reset(navigation, "Welcome");
     }
 
-    renderHeader(): React.Node {
-        const profile = APIStore.profile(APIStore.me());
-        return (
-            <View style={styles.header}>
-                <Image style={styles.cover} source={Images.cover} />
-                <View style={styles.logout}>
-                    <TouchableOpacity onPress={this.logout}>
-                        <View>
-                            <Icon name="log-out" size={25} color="white" />
-                        </View>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.headerContent}>
-                    <View style={styles.title}>
-                        <Text type="large" style={styles.outline}>{profile.outline}</Text>
-                        <Text type="header2" style={styles.name}>{profile.name}</Text>
-                    </View>
-                    <Avatar size={avatarSize} style={styles.avatar} {...profile.picture} />
-                </View>
-            </View>
-        );
-    }
-
     render(): React.Node {
         const {navigation} = this.props;
         const uid = APIStore.me();
         const posts = APIStore.posts().filter(post => post.uid === uid);
+        const profile = APIStore.profile(uid);
+        const {scrollAnimation} = this.state;
+        const height = scrollAnimation.interpolate({
+            inputRange: [0, 0, 100, 100],
+            outputRange: [width, width, statusBarHeight + 100, statusBarHeight + 100]
+        });
+        const opacity = scrollAnimation.interpolate({
+            inputRange: [0, 0, 100, 100],
+            outputRange: [1, 1, 0, 0]
+        });
         return (
-            <SafeAreaView style={styles.container}>
+            <View style={styles.container}>
+                <Animated.View style={[styles.header, { height } ]}>
+                    <AnimatedImage style={[styles.cover, { height }]} source={Images.cover} />
+                    <TouchableOpacity onPress={this.logout} style={styles.settings}>
+                        <View>
+                            <Icon name="log-out" size={25} color="white" />
+                        </View>
+                    </TouchableOpacity>
+                    <Animated.View style={[styles.title, { opacity }]}>
+                        <Text type="large" style={styles.outline}>{profile.outline}</Text>
+                        <Text type="header2" style={styles.name}>{profile.name}</Text>
+                    </Animated.View>
+                    <Avatar size={avatarSize} style={styles.avatar} {...profile.picture} />
+                </Animated.View>
                 <FlatList
                     showsVerticalScrollIndicator={false}
                     style={styles.list}
                     data={posts}
                     keyExtractor={post => post.id}
-                    renderItem={({ item }) => (
-                        <View style={styles.post}>
-                            <Post post={item} {...{navigation}} />
-                        </View>
-                    )}
-                    ListEmptyComponent={(
-                        <View style={styles.post}>
-                            <FirstPost {...{navigation}} />
-                        </View>
-                    )}
-                    ListHeaderComponent={this.renderHeader()}
+                    renderItem={({ item }) => <Post post={item} {...{navigation}} />}
+                    ListEmptyComponent={<FirstPost {...{navigation}} />}
+                    onScroll={Animated.event([{
+                        nativeEvent: {
+                            contentOffset: {
+                                y: scrollAnimation
+                            }
+                        }
+                    }])}
                 />
-            </SafeAreaView>
+            </View>
         );
     }
 }
 
 const avatarSize = 100;
 const {width} = Dimensions.get("window");
+const {statusBarHeight} = Constants;
+const AnimatedImage = Animated.createAnimatedComponent(Image);
 const styles = StyleSheet.create({
     container: {
         flex: 1
     },
-    header: {
-        marginBottom: 50
+    list: {
+        flex: 1,
+        paddingHorizontal: Theme.spacing.small
     },
-    headerContent: {
-        ...StyleSheet.absoluteFillObject
+    header: {
+        marginBottom: avatarSize * 0.5 + Theme.spacing.small
     },
     cover: {
-        height: width
+        ...StyleSheet.absoluteFillObject,
+        width
     },
     avatar: {
         position: "absolute",
         right: Theme.spacing.small,
-        bottom: -50
+        bottom: - avatarSize * 0.5
+    },
+    settings: {
+        position: "absolute",
+        top: statusBarHeight + Theme.spacing.small,
+        right: Theme.spacing.base,
+        backgroundColor: "transparent",
+        zIndex: 10000
     },
     title: {
         position: "absolute",
         left: Theme.spacing.small,
-        bottom: Theme.spacing.small
-    },
-    logout: {
-        position: "absolute",
-        top: Theme.spacing.small,
-        right: Theme.spacing.base,
-        backgroundColor: "transparent",
-        zIndex: 10000
+        bottom: 50 + Theme.spacing.small
     },
     outline: {
         color: "rgba(255, 255, 255, 0.8)"
     },
     name: {
         color: "white"
-    },
-    list: {
-        paddingBottom: 57 + Theme.spacing.small
-    },
-    post: {
-        paddingHorizontal: Theme.spacing.small
     }
 });
